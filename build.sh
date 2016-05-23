549cf89923c04f4d010bf985325d77bf939eb5ad
Jason Trost <jason.trost@gmail.com>
2016-05-23 16:34:26 -0400

    fixed build script

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@
 
 PACKAGE=mhn-splunk.spl
 
+export COPYFILE_DISABLE=true
 rm -f $PACKAGE
-tar --exclude=mhn-splunk/.git/ -zcvf $PACKAGE mhn-splunk/ 
+tar --exclude=".*" --exclude=mhn-splunk/.git/ -zcvf $PACKAGE mhn-splunk/ 
 
 echo ""
 echo "Finished building $PACKAGE"
